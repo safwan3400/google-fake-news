f41bdd48a83a0f3c2141e5473175313c464c16e5
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-02-19 16:41:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, abort, jsonify, request, render_template
+import joblib
+from feature import *
+import json
+import os
+import webbrowser
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/api',methods=['POST'])
+def get_delay():
+	result=request.form
+	query_title = result['title']
+	query_author = result['author']
+	query_text = result['maintext']
+	lol = query_title + " " + query_author + " " + query_text
+	link =	'https://factchecktools.googleapis.com/v1alpha1/claims:search?query=' + str(lol) + '&key=AIzaSyAKjLlNdhqE_bjTbFRybC7yXR_7ht0ZspA'
+	return webbrowser.open(link)
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
