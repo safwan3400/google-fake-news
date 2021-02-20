f489970077d4baec435616942069118daba94ea6
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-02-20 09:51:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_delay():
 	query_text = result['maintext']
 	lol = query_title + " " + query_author + " " + query_text
 	link =	'https://factchecktools.googleapis.com/v1alpha1/claims:search?query=' + str(lol) + '&key=AIzaSyAKjLlNdhqE_bjTbFRybC7yXR_7ht0ZspA'
-	return webbrowser.open("https://google-fake-news.herokuapp.com/")
+	return os.system(link)
 
 
 
