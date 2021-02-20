e93b4d39972c9063f69fdb0196171c3cda1a6276
Mohammed safwan <53251928+safwan3400@users.noreply.github.com>
2021-02-20 09:40:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def get_delay():
 	query_author = result['author']
 	query_text = result['maintext']
 	lol = query_title + " " + query_author + " " + query_text
-	link =	'https://factchecktools.googleapis.com/v1alpha1/claims:search?query=' + str(lol) + '&key=AIzaSyAKjLlNdhqE_bjTbFRybC7yXR_7ht0ZspA'
+	link =	'https://factchecktools.googleapis.com/v1alpha1/claims:search?query='" + str(lol) + "'&key=AIzaSyAKjLlNdhqE_bjTbFRybC7yXR_7ht0ZspA'
 	return webbrowser.open(link)
 
 
